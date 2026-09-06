cce99f227df6f633f62c6c0660a66e23c97130cd
gaurigoyal615-lgtm <gaurigoyal615@gmail.com>
2026-09-07 03:04:21 +0530

    demonstrating sequential scanning time

--- client.py
+++ client.py
@@ -1,6 +1,7 @@
 import argparse
 import sys
 import socket
+import time
 parser= argparse.ArgumentParser()
 parser.add_argument("target")
 parser.add_argument("--start", type=int)
@@ -20,16 +21,20 @@ def check_port(target, port):
     except TimeoutError:
         return "TIMEOUT"
     except ConnectionError as ce:
-        return ce
+        return "ERROR"
     except OSError as e:
-       return e
+       return "ERROR"
     finally:
         client_socket.close()
 if(start_port<=0 or end_port<=0 or start_port> end_port or  start_port>= 65536 or end_port>=65536 ):
     print("Provide valid start and end port")
     sys.exit()
+start = time.perf_counter()
 for i in range(start_port, end_port+1):
     result = check_port(HOST, i)
-    
+    if result== "OPEN":
+        print(f"Port {i}: OPEN")
+end= time.perf_counter()
+print(f"Scan complete in {round(end-start, 3)} seconds")
 
 
