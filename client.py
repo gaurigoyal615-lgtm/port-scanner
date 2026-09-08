d445214f85143f308c14fbfafe2c9feb61298215
gaurigoyal615-lgtm <gaurigoyal615@gmail.com>
2026-09-09 01:58:17 +0530

    added  workers and timeout as CLI args

--- client.py
+++ client.py
@@ -7,13 +7,17 @@ parser= argparse.ArgumentParser()
 parser.add_argument("target")
 parser.add_argument("--start", type=int)
 parser.add_argument("--end", type=int)
+parser.add_argument("--workers", type=int)
+parser.add_argument("--timeout", type=float)
 args= parser.parse_args()
 HOST = args.target
 start_port= args.start
+timeout= args.timeout
 end_port= args.end
-def check_port(target, port): 
+def check_port(target, port,timeout): 
     client_socket= socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client_socket.settimeout(2)
+    
+    client_socket.settimeout(timeout)
     try:
         client_socket.connect((target, port))
         return "OPEN"
@@ -28,14 +32,15 @@ def check_port(target, port):
     finally:
         client_socket.close()
 
-if(start_port<=0 or end_port<=0 or start_port> end_port or  start_port>= 65536 or end_port>=65536 ):
-    print("Provide valid start and end port")
+if(start_port<=0 or end_port<=0 or start_port> end_port or  start_port>= 65536 or end_port>=65536 or timeout<=0 ):
+    print("Provide valid start and end port or timeout")
     sys.exit()
+
 start = time.perf_counter()
-with ThreadPoolExecutor(max_workers=10) as executor:
+with ThreadPoolExecutor(max_workers=args.workers) as executor:
     future_to_port= {}
     for i in range(start_port, end_port+1):
-        future= executor.submit(check_port, HOST, i)
+        future= executor.submit(check_port, HOST, i, timeout)
         future_to_port[future]= i
     for future in as_completed(future_to_port):
         port = future_to_port[future]
